3cc98885dc0274f37f238a4ce1469b1e570bfc5b
maya miyoshi <m.miyoshi.carecon@gmail.com>
2026-04-30 16:27:21 +0900

    作品登録画面ちょっといじった

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
     protected $fillable = ['name'];
+
+    public function works()
+    {
+        return $this->belongsToMany(Work::class);
+    }
 }
--- resources/views/admin/works/create.blade.php
+++ resources/views/admin/works/create.blade.php
@@ -35,12 +35,13 @@
         </div>
 
         <div class="label">ジャンル</div>
-        <div class="input">
-            <select name="genre_id">
-                @foreach($genres as $genre)
-                <option value="{{ $genre->id }}">{{ $genre->name }}</option>
-                @endforeach
-            </select>
+        <div class="input checkbox-group">
+            @foreach($genres as $genre)
+            <label>
+                <input type="checkbox" name="genre_ids[]" value="{{ $genre->id }}">
+                {{ $genre->name }}
+            </label>
+            @endforeach
         </div>
 
         <div class="label">作品イメージ</div>
@@ -50,9 +51,27 @@
             <button type="button" id="remove-image" style="display:none;">削除</button>
         </div>
 
-        <div class="label">公開年</div>
-        <div class="input">
-            <input type="number" name="release_year">
+        <div class="label">公開情報</div>
+        <div class="input inline-group">
+            <div>
+                <label>年</label>
+                <input type="number" name="release_year">
+            </div>
+
+            <div>
+                <label>月</label>
+                <input type="number" name="release_month" min="1" max="12">
+            </div>
+
+            <div>
+                <label>日</label>
+                <input type="number" name="release_day" min="1" max="31">
+            </div>
+
+            <div>
+                <label>上映時間（分）</label>
+                <input type="number" name="duration">
+            </div>
         </div>
 
         <div class="label">制作国</div>
@@ -62,6 +81,60 @@
                 <option value="{{ $country->id }}">{{ $country->name }}</option>
                 @endforeach
             </select>
+        </div>
+
+        <div class="label">監督</div>
+        <div class="input">
+            <input type="text" id="director-input" size="40">
+            <button type="button" id="add-director-btn">追加</button>
+            <ul id="director-suggestions"></ul>
+
+            <table border="1" id="director-table" style="display: none;">
+                <thead>
+                    <tr>
+                        <th>監督名</th>
+                        <th>順序</th>
+                        <th>削除</th>
+                    </tr>
+                </thead>
+                <tbody id="director-list"></tbody>
+            </table>
+        </div>
+
+        <div class="label">原作</div>
+        <div class="input">
+            <input type="text" id="original-input" size="40">
+            <button type="button" id="add-original-btn">追加</button>
+            <ul id="original-suggestions"></ul>
+
+            <table border="1" id="original-table" style="display: none;">
+                <thead>
+                    <tr>
+                        <th>原作者</th>
+                        <th>順序</th>
+                        <th>削除</th>
+                    </tr>
+                </thead>
+                <tbody id="original-list"></tbody>
+            </table>
+        </div>
+
+        <div class="label">主題歌</div>
+        <div class="input">
+            <input type="text" id="song-input" size="40">
+            <button type="button" id="add-song-btn">追加</button>
+            <ul id="song-suggestions"></ul>
+
+            <table border="1" id="song-table" style="display: none;">
+                <thead>
+                    <tr>
+                        <th>歌手名</th>
+                        <th>順序</th>
+                        <th>削除</th>
+                    </tr>
+                </thead>
+                <tbody id="song-list"></tbody>
+            </table>
         </div>
 
         <div class="label">キャスト名</div>
@@ -86,6 +159,11 @@
         <div class="input">
             <input type="text" id="maker-input" name="maker_name" size="40">
             <ul id=maker-suggestion></ul>
+        </div>
+
+        <div class="label">あらすじ</div>
+        <div class="input">
+            <textarea name="description" rows="4"></textarea>
         </div>
 
         <div class="label">賞</div>
